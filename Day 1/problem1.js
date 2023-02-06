17940c40e9f2dba0d456196d20ebe8612b106c94
Lalit26135 <melalit625@gmail.com>
2023-02-06 09:55:09 +0530

    first commit

---
+++ Day 1/problem1.js
@@ -0,0 +1,4 @@
+let number = 6;
+if((number%3)==0){
+  console.log("number is multiple of 3");
+}
---
+++ Day 1/problem4.js
@@ -0,0 +1,21 @@
+let name="Lalit Sengar";
+let school="jnv hathras";
+let grade="A";
+let sec="B";
+let rollno = 135;
+let maths = 48;
+let english = 46;
+let sci= 49;
+
+console.log("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+console.log("@**************** SCORE CARD ***************@");
+console.log("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+console.log("@ Name =",name,"  School=",school,"@");
+console.log("@ Section =",sec,"          Roll no. = ",rollno,"    @");
+console.log("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+console.log("@************* Marks & Grades **************@");
+console.log("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+console.log("@ Maths   = ",maths,"            ",grade,"             @");
+console.log("@ English = ",english,"            ",grade,"             @");
+console.log("@ Sciense = ",sci,"            ",grade,"             @");
+console.log("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
